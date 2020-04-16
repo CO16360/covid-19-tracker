fb99525b2086a3a832df6c99dfb7f6150cb684f4
vikasco16360 <poonervikas98@gmail.com>
2020-04-16 13:08:43 +0530

    Updates

---
+++ precache-manifest.38900ec1915a563ade59ff305bf98ec0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a6cd385567e84c4c4ab3b1ddbb02caa",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "354880a16e2dce8f54b0",
+    "url": "/covid-19-tracker/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "447a74d2e887f81608f0",
+    "url": "/covid-19-tracker/static/css/main.b5f27ea4.chunk.css"
+  },
+  {
+    "revision": "354880a16e2dce8f54b0",
+    "url": "/covid-19-tracker/static/js/2.628a51a4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/covid-19-tracker/static/js/2.628a51a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "447a74d2e887f81608f0",
+    "url": "/covid-19-tracker/static/js/main.1616fefd.chunk.js"
+  },
+  {
+    "revision": "2695bed86988f5301848",
+    "url": "/covid-19-tracker/static/js/runtime-main.789ade28.js"
+  }
+]);
